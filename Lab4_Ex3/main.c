b55cbae14221566c161c25cce05ab546c12624e3
Christos Karolemeas <ckarol@christoss-mbp>
2016-08-31 13:36:40 +0300

    Initial Commit

---
+++ Lab4_Ex3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab4_Ex3
+//
+//  Created by Christos Karolemeas on 31/08/16.
+//  Copyright © 2016 Christos Karolemeas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
